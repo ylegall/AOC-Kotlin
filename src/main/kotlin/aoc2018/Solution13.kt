36d4416edf3e05cf7050ccf7c4f8b58225ca55da
Yann Le Gall <ylegall@gmail.com>
2018-12-22 17:54:32 -0800

    adds Day 13 part 1

---
+++ src/main/kotlin/aoc2018/Solution13.kt
@@ -0,0 +1,108 @@
+package aoc2018
+
+import Point
+import java.io.FileInputStream
+
+enum class Direction {
+    UP, RIGHT, DOWN, LEFT
+}
+
+enum class TurnType(val value: Int) {
+    LEFT(-1), STRAIGHT(0), RIGHT(1)
+}
+
+private fun TurnType.nextTurn(): TurnType {
+    return TurnType.values()[(ordinal + 1) % TurnType.values().size]
+}
+
+private fun Direction.turn(turn: TurnType): Direction {
+    val newDirection = Direction.values().size + this.ordinal + turn.value
+    return Direction.values()[newDirection % Direction.values().size]
+}
+
+private class Car(
+    var pos: Point,
+    var direction: Direction,
+    var turn: TurnType = TurnType.LEFT
+) {
+    override fun toString() = when(direction) {
+        Direction.UP -> "^"
+        Direction.RIGHT -> ">"
+        Direction.DOWN -> "v"
+        Direction.LEFT -> "<"
+    }
+}
+
+private fun Car.advance() {
+    pos = when (direction) {
+        Direction.UP -> Point(pos.x, pos.y - 1)
+        Direction.RIGHT -> Point(pos.x + 1, pos.y)
+        Direction.LEFT -> Point(pos.x - 1, pos.y)
+        Direction.DOWN -> Point(pos.x, pos.y + 1)
+    }
+}
+
+private fun Car.turn(roads: Map<Point, Char>) {
+    val road = roads[pos] ?: return
+    when (road) {
+        '/' -> direction = when (direction) {
+            Direction.UP -> Direction.RIGHT
+            Direction.LEFT -> Direction.DOWN
+            Direction.RIGHT -> Direction.UP
+            Direction.DOWN -> Direction.LEFT
+        }
+        '\\' -> direction = when (direction) {
+            Direction.UP -> Direction.LEFT
+            Direction.LEFT -> Direction.UP
+            Direction.RIGHT -> Direction.DOWN
+            Direction.DOWN -> Direction.RIGHT
+        }
+        '+' -> {
+            direction = direction.turn(turn)
+            turn = turn.nextTurn()
+        }
+    }
+}
+
+private fun getFirstCrash(roads: Map<Point, Char>, cars: HashMap<Point, Car>): Point {
+    var i = 1
+    while (true) {
+        cars.entries.sortedWith(compareBy({ it.key.y }, { it.key.x })).forEach { entry ->
+            val car = entry.value
+            cars.remove(car.pos)
+            car.advance()
+            if (car.pos in cars) {
+                return car.pos
+            }
+            cars[car.pos] = car
+            car.turn(roads)
+        }
+        println(i)
+        i += 1
+    }
+}
+
+fun main() {
+    val roads = HashMap<Point, Char>()
+    val cars = HashMap<Point, Car>()
+
+    FileInputStream("inputs/2018/13.txt").bufferedReader().use {
+        var y = 0
+        for (line in it.lines()) {
+            var x = 0
+            for (c in line) {
+                when (c) {
+                    '+','\\','/' -> roads[Point(x,y)] = c
+                    '>' -> Car(Point(x, y), Direction.RIGHT).apply { cars[pos] = this }
+                    '^' -> Car(Point(x, y), Direction.UP).apply { cars[pos] = this }
+                    'v' -> Car(Point(x, y), Direction.DOWN).apply { cars[pos] = this }
+                    '<' -> Car(Point(x, y), Direction.LEFT).apply { cars[pos] = this }
+                }
+                x += 1
+            }
+            y += 1
+        }
+        println(getFirstCrash(roads, cars))
+    }
+
+}
