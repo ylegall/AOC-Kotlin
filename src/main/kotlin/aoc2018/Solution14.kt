ff893281dbac9b623c3030c406f4f8eaa18a7810
Yann Le Gall <ygall@indeed.com>
2018-12-23 17:43:48 -0800

    minor performance optimizations to Solution 14

--- src/main/kotlin/aoc2018/Solution14.kt
+++ src/main/kotlin/aoc2018/Solution14.kt
@@ -1,13 +1,25 @@
 package aoc2018
 
+import kotlin.system.measureTimeMillis
+
 const val INPUT = 681901
 
-private fun nextRecipes(a: Int, b: Int): List<Int> {
-    return (a + b).toString().map { Character.getNumericValue(it) }
+private val next = ArrayList<Byte>(2)
+
+private fun nextRecipes(a: Byte, b: Byte): List<Byte> {
+    next.clear()
+    val result = (a + b).toByte()
+    if (result < 10) {
+        next.add(result)
+    } else {
+        next.add(1)
+        next.add((result % 10).toByte())
+    }
+    return next
 }
 
 private fun lastTenRecipesAfter(index: Int): String {
-    val recipes = arrayListOf(3, 7)
+    val recipes = arrayListOf<Byte>(3, 7)
     var a = 0
     var b = 1
     while (recipes.size < index + 10) {
@@ -18,8 +30,8 @@ private fun lastTenRecipesAfter(index: Int): String {
     return recipes.takeLast(10).joinToString("")
 }
 
-private fun numRecipesBefore(targetSequence: List<Int>): Int {
-    val recipes = arrayListOf(3, 7)
+private fun numRecipesBefore(targetSequence: List<Byte>): Int {
+    val recipes = arrayListOf<Byte>(3, 7)
     var a = 0
     var b = 1
     while (true) {
@@ -36,6 +48,9 @@ private fun numRecipesBefore(targetSequence: List<Int>): Int {
 }
 
 fun main() {
-    println(lastTenRecipesAfter(INPUT))
-    println(numRecipesBefore(INPUT.toString().map { Character.getNumericValue(it) }))
+    val elapsed = measureTimeMillis {
+        println(lastTenRecipesAfter(INPUT))
+        println(numRecipesBefore(INPUT.toString().map { Character.getNumericValue(it).toByte() }))
+    }
+    println(elapsed)
 }
