2b8d96e74ab2062dc513e1ef69341a3e9290ff73
Yann Le Gall <ylegall@gmail.com>
2018-12-26 19:54:53 -0800

    adds comparator to Point class

--- src/main/kotlin/util/Point.kt
+++ src/main/kotlin/util/Point.kt
@@ -1,6 +1,13 @@
 package util
 
-data class Point(val x: Int, val y: Int)
+data class Point(
+        val x: Int,
+        val y: Int
+): Comparable<Point> {
+    override fun compareTo(other: Point) = pointComparator.compare(this, other)
+}
+
+val pointComparator = compareBy<Point>({ it.y }, { it.x })
 
 fun Point.mDist(x: Int, y: Int): Int {
     return Math.abs(this.x - x) + Math.abs(this.y - y)
