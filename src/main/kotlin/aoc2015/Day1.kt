ef3b29152026810fb3bd0b766226506ef4071e80
Yann Le Gall <ylegall@gmail.com>
2019-01-03 21:03:49 -0800

    adds 2015 day 1

---
+++ src/main/kotlin/aoc2015/Day1.kt
@@ -0,0 +1,26 @@
+package aoc2015
+
+import util.input
+import kotlin.streams.toList
+
+fun countParens(parens: String): Int {
+    return parens.map { if (it == '(') 1 else -1 }.sum()
+}
+
+tailrec fun firstNegativeIndex(index: Int, sum: Int, parens: String): Int {
+    if (index > parens.length) return -1
+    if (sum == -1) return index
+    val newSum = sum + if (parens[index] == '(') 1 else -1
+    return firstNegativeIndex(index + 1, newSum, parens)
+}
+
+fun main() {
+    // part 1
+    val parens = input("inputs/2015/1.txt").use {
+        it.toList().joinToString("").filter { it != '(' || it != ')' }
+    }
+    println(countParens(parens))
+
+    // part 2
+    println(firstNegativeIndex(0, 0, parens))
+}
