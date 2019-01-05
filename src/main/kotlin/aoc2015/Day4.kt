27479b4a340c09113573787cea04c5965422599d
Yann Le Gall <ylegall@gmail.com>
2019-01-05 09:54:43 -0800

    adds 2015 day 4

---
+++ src/main/kotlin/aoc2015/Day4.kt
@@ -0,0 +1,23 @@
+package aoc2015
+
+import java.security.MessageDigest
+import javax.xml.bind.DatatypeConverter
+
+
+const val INPUT = "iwrupvqb"
+
+private fun firstHashWithPrefix(key: String, target: String): Int {
+    val md = MessageDigest.getInstance("MD5")
+    return generateSequence(1) { it + 1 }.first {
+        val input = "$key$it".toByteArray()
+        DatatypeConverter.printHexBinary(md.digest(input)).startsWith(target)
+    }
+}
+
+fun main() {
+    // part 1:
+    println(firstHashWithPrefix(INPUT, "00000"))
+
+    // part 2:
+    println(firstHashWithPrefix(INPUT, "000000"))
+}
