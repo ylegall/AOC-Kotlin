3675248d8df921cd15dc0281166c2b7d83427b09
Yann Le Gall <ylegall@gmail.com>
2019-10-06 18:42:13 -0700

    adds 2017 day 7

--- src/main/kotlin/aoc2017/Day7.kt
+++ src/main/kotlin/aoc2017/Day7.kt
@@ -1,10 +1,21 @@
 package aoc2017
 
 import util.input
-import kotlin.time.ExperimentalTime
-import kotlin.time.measureTimedValue
 
 private object Day7 {
+
+    private data class Node(
+            val name: String,
+            val weight: Int
+    )
+
+    private class WeirdTree(
+            val root: String,
+            val nodes: Map<String, Node>,
+            val children: Map<String, List<String>>
+    ) {
+        fun children(name: String) = children[name]?.mapNotNull { nodes[it] } ?: emptyList()
+    }
 
     private fun findRoot(nodes: MutableMap<String, List<String>>): String {
         while (nodes.size > 1) {
@@ -19,32 +30,79 @@ private object Day7 {
         }
         return nodes.keys.first()
     }
-
-    private fun parseInput(): MutableMap<String, List<String>> {
-        val nodes = mutableMapOf<String, List<String>>()
-        input("inputs/2017/7.txt").use { lines ->
-            lines.forEach { line ->
-                val parts = line.split("->")
-                val parent = parts[0].slice(0 until parts[0].indexOf(' '))
-                if (parts.size > 1) {
-                    val children = parts[1].split(',').map { it.trim() }
-                    nodes[parent] = children
-                } else {
-                    nodes[parent] = emptyList()
-                }
-            }
-        }
-        return nodes
+    private fun findWeightDiscrepancy(tree: WeirdTree): Int {
+        val correctedWeights = ArrayList<Int>()
+        findWeightDiscrepancy(tree.root, tree, correctedWeights)
+        return correctedWeights.first()
     }
 
-    @ExperimentalTime
+    private fun findWeightDiscrepancy(
+            node: String,
+            tree: WeirdTree,
+            correctedWeights: ArrayList<Int>
+    ): Int {
+        val nodeWeight = tree.nodes[node]?.weight ?: 0
+        val children = tree.children(node)
+        return if (children.isEmpty()) {
+            nodeWeight
+        } else {
+            val weights = children.map { it to findWeightDiscrepancy(it.name, tree, correctedWeights) }
+            val outlier = findNodeWithWrongWeight(weights)
+            if (outlier != null) {
+                if (correctedWeights.isEmpty()) {
+                    println("\nfound weight outlier in children of $node!")
+                    //println(weights)
+                    println("outlier node: ${outlier.first}")
+                    println("weight difference: ${outlier.second}")
+                    println("correct weight should be: " + (outlier.first.weight + outlier.second))
+                    correctedWeights.add(outlier.first.weight + outlier.second)
+                }
+            }
+            val childWeights = weights.map { it.second }.sum()
+            val totalSubtreeWeight = nodeWeight + childWeights
+            totalSubtreeWeight
+        }
+    }
+
+    private fun findNodeWithWrongWeight(nodes: List<Pair<Node, Int>>): Pair<Node, Int>? {
+        val weightCounts = nodes.groupingBy { it.second }.eachCount()
+        return when {
+            weightCounts.size == 1 -> null
+            weightCounts.size == 2 -> {
+                val correctWeight = weightCounts.maxBy { it.value }?.key!!
+                val wrongWeight = weightCounts.minBy { it.value }?.key!!
+                val wrongNode = nodes.find { it.second != correctWeight }!!.first
+                wrongNode to correctWeight - wrongWeight
+            }
+            else -> throw Exception("invalid weight distributions: $nodes")
+        }
+    }
+
+    private fun parseInput(): WeirdTree {
+        val children = mutableMapOf<String, List<String>>()
+        val nodes = mutableMapOf<String, Node>()
+
+        input("inputs/2017/7.txt").use { lines ->
+            lines.forEach { line ->
+                val tokens = line.split("(", "->", ")", ",").map { it.trim() }.filter { it.isNotEmpty() }
+                if (tokens.size > 2) {
+                    children[tokens[0]] = tokens.drop(2)
+                }
+                nodes[tokens[0]] = Node(tokens[0], tokens[1].toInt())
+            }
+        }
+
+        val root = findRoot(children.toMutableMap())
+        return WeirdTree(root, nodes, children)
+    }
+
     fun run() {
-        val input = parseInput()
-        println(measureTimedValue { findRoot(input) })
+        val tree = parseInput()
+        println(tree.root)
+        println(findWeightDiscrepancy(tree))
     }
 }
 
-@ExperimentalTime
 fun main() {
     Day7.run()
 }
