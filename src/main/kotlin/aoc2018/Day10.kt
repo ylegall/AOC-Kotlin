33a385b788273b29d3e9cf79ef6969dd9eff9651
Yann Le Gall <ylegall@gmail.com>
2019-11-23 09:04:22 -0800

    refactors 2018 day 10

---
+++ src/main/kotlin/aoc2018/Day10.kt
@@ -0,0 +1,62 @@
+package aoc2018
+
+import java.io.File
+
+private object Day10 {
+
+    private const val LIMIT = 20000
+
+    private data class Vec(var x: Int, var y: Int)
+
+    private fun findMessage(positions: List<Vec>, velocities: List<Vec>) {
+
+        var minH = Int.MAX_VALUE
+        var minW = Int.MAX_VALUE
+
+        for (t in 0 ..LIMIT) {
+
+            var minX = positions[0].x
+            var minY = positions[0].y
+            var maxX = positions[0].x
+            var maxY = positions[0].y
+
+            for (i in positions.indices) {
+                positions[i].x += velocities[i].x
+                positions[i].y += velocities[i].y
+
+                if (positions[i].x < minX) minX = positions[i].x
+                else if (positions[i].x > maxX) maxX = positions[i].x
+
+                if (positions[i].y < minY) minY = positions[i].y
+                else if (positions[i].y > maxY) maxY = positions[i].y
+            }
+
+            val w = maxX - minX + 1
+            val h = maxY - minY + 1
+
+            if (w < minW) minW = w
+            if (h < minH) minH = h
+
+            println("$t: area: ${w * h}, minW: $minW, minH: $minH")
+        }
+    }
+
+    fun run() {
+        val positions = arrayListOf<Vec>()
+        val velocities = arrayListOf<Vec>()
+
+        File("inputs/2018/10.txt").useLines { lines ->
+            for (line in lines) {
+                val tokens = line.split(',', '<', '>').map { it.trim() }
+                positions.add(Vec(tokens[1].toInt(), tokens[2].toInt()))
+                velocities.add(Vec(tokens[4].toInt(), tokens[5].toInt()))
+            }
+        }
+
+        findMessage(positions, velocities)
+    }
+}
+
+fun main() {
+    Day10.run()
+}
