30ce20ff85c1722f87dc0dcbaf796885da9a05ac
Yann Le Gall <ygall@indeed.com>
2019-11-25 18:41:49 -0800

    adds union find utility

--- src/main/kotlin/aoc2017/Day12.kt
+++ src/main/kotlin/aoc2017/Day12.kt
@@ -1,48 +1,9 @@
 package aoc2017
 
+import util.UnionFind
 import util.input
 import kotlin.streams.asSequence
 
-private class UnionFind(initialSize: Int) {
-
-    private var sizes = Array(initialSize) { 1 }
-    private val groups = Array(initialSize) { it }
-
-    var size = initialSize
-        private set
-
-    fun size(item: Int) = sizes[find(item)]
-
-    fun find(item: Int): Int {
-        var root = item
-        while (root != groups[root]) {
-            root = groups[root]
-        }
-        var node = item
-        while (node != root) {
-            val next = groups[node]
-            groups[node] = root
-            node = next
-        }
-        return root
-    }
-
-    fun union(item1: Int, item2: Int) {
-        val root1 = find(item1)
-        val root2 = find(item2)
-
-        if (root1 == root2) return
-
-        if (sizes[root1] > sizes[root2]) {
-            groups[root1] = root2
-            sizes[root2] += sizes[root1]
-        } else {
-            groups[root2] = root1
-            sizes[root1] += sizes[root2]
-        }
-        size--
-    }
-}
 
 private fun parseLine(line: String): Pair<Int, List<Int>> {
     val tokens = line.split(" <-> ")
