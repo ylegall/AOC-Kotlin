407254fcf8cdf782149f176cc5ca6a88e150488e
Yann Le Gall <ygall@indeed.com>
2019-12-01 10:45:17 -0800

    refactors 2016 day 1

--- src/main/kotlin/aoc2016/Day1.kt
+++ src/main/kotlin/aoc2016/Day1.kt
@@ -1,68 +1,72 @@
 package aoc2016
 
 import util.Point
-import util.input
 import util.mDist
 import util.plus
 import util.times
 import java.io.File
-import kotlin.streams.toList
 
-private val directions = arrayOf(
-    Point(0, -1),
-    Point(1, 0),
-    Point(0, 1),
-    Point(-1, 0)
-)
-
-private fun Point.move(start: Point, mag: Int) = (this * mag) + start
-
-private class State(
-        val pos: Point,
-        val dir: Point,
-        val visited: Set<Point> = setOf(),
-        val endPoint: Point? = null
-)
-
-private fun Point.turn(c: Char) = when(c) {
-    'L' -> -1
-    'R' -> 1
-    else -> throw Exception("you might be going in a bad direction")
-}.let {
-    directions[(directions.size + directions.indexOf(this) + it) % directions.size]
-}
-
-private fun State.moveBy(nextMove: String): State {
-    val newDirection = dir.turn(nextMove.first())
-    val magnitude = nextMove.drop(1).toInt()
-    val newPosition = newDirection.move(pos, magnitude)
-    val newVisited = expandPoints(this.pos, newPosition, newDirection)
-    return State(
-            newPosition,
-            newDirection,
-            visited + newVisited,
-            endPoint ?: newVisited.asSequence().find(visited::contains)
+private object Day1 {
+    private val directions = arrayOf(
+            Point(0, -1),
+            Point(1, 0),
+            Point(0, 1),
+            Point(-1, 0)
     )
-}
 
-private fun expandPoints(start: Point, end: Point, direction: Point): List<Point> {
-    return generateSequence(start) {
-        it + direction
-    }.takeWhile {
-        it != end
-    }.toList()
-}
+    private fun Point.move(start: Point, mag: Int) = (this * mag) + start
 
-private fun distanceFromHQ(moves: List<String>) = moves.fold(
-        State(Point(0, 0), directions[0]),
-        State::moveBy
-)
+    private class State(
+            val pos: Point,
+            val dir: Point,
+            val visited: Set<Point> = setOf(),
+            val endPoint: Point? = null
+    ) {
+        fun moveBy(nextMove: String): State {
+            val newDirection = dir.turn(nextMove.first())
+            val magnitude = nextMove.drop(1).toInt()
+            val newPosition = newDirection.move(pos, magnitude)
+            val newVisited = expandPoints(this.pos, newPosition, newDirection)
+            return State(
+                    newPosition,
+                    newDirection,
+                    visited + newVisited,
+                    endPoint ?: newVisited.asSequence().find(visited::contains)
+            )
+        }
+    }
+
+    private fun Point.turn(c: Char) = when(c) {
+        'L' -> -1
+        'R' -> 1
+        else -> throw Exception("you might be going in a bad direction")
+    }.let {
+        directions[(directions.size + directions.indexOf(this) + it) % directions.size]
+    }
+
+    private fun expandPoints(start: Point, end: Point, direction: Point): List<Point> {
+        return generateSequence(start) {
+            it + direction
+        }.takeWhile {
+            it != end
+        }.toList()
+    }
+
+    private fun distanceFromHQ(moves: List<String>) = moves.fold(
+            State(Point(0, 0), directions[0]),
+            State::moveBy
+    )
+
+    fun run() {
+        val directions = File("inputs/2016/1.txt").useLines {
+            it.toList().first().split(", ")
+        }
+        val state = distanceFromHQ(directions)
+        println(state.pos.mDist(Point(0,0)))
+        println(state.endPoint!!.mDist(Point(0,0)))
+    }
+}
 
 fun main() {
-    val directions = File("inputs/2016/1.txt").useLines {
-        it.toList().first().split(", ")
-    }
-    val state = distanceFromHQ(directions)
-    println(state.pos.mDist(Point(0,0)))
-    println(state.endPoint!!.mDist(Point(0,0)))
+    Day1.run()
 }
