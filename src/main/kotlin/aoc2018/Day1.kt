515979715793fe30ae898696f3075e91bd2ead55
Yann Le Gall <ygall@indeed.com>
2019-12-01 10:29:40 -0800

    refactors 2018 day 1

--- src/main/kotlin/aoc2018/Day1.kt
+++ src/main/kotlin/aoc2018/Day1.kt
@@ -1,24 +1,24 @@
 package aoc2018
 
-import util.input
-import kotlin.streams.toList
+import util.CircularList
+import java.io.File
 
-private fun firstRepeat(list: Iterable<Int>): Int {
-    var sum = 0
+
+private fun firstRepeat(list: List<Int>): Int {
     val seen = hashSetOf(0)
-    while (true) {
-        for (item in list) {
-            sum += item
-            if (sum in seen) {
-                return sum
-            }
-            seen.add(sum)
+    var sum = 0
+    for (item in CircularList(list)) {
+        sum += item
+        if (sum in seen) {
+            break
         }
+        seen.add(sum)
     }
+    return sum
 }
 
 fun main() {
-    val list = input("inputs/2018/1.txt").use { lines ->
+    val list = File("inputs/2018/1.txt").useLines { lines ->
         lines.map { it.toInt() }.toList()
     }
     println(list.sum())
