754c530c149316e9eaac04946ad6512704efc205
Yann Le Gall <ylegall@gmail.com>
2019-12-04 17:17:53 -0800

    refactors 2019 day 4

--- src/main/kotlin/aoc2019/Day4.kt
+++ src/main/kotlin/aoc2019/Day4.kt
@@ -22,30 +22,15 @@ private fun Int.hasRepeatOfAtLeast2() = digitList().zipWithNext().any { (first, 
     first == second
 }
 
-private fun Int.hasRepeatOfExactly2(): Boolean {
-    val digits = digitList()
-    for (i in 1 until digits.size) {
-        val a = digits.getOrNull(i - 2)
-        val b = digits.getOrNull(i - 1)
-        val c = digits.getOrNull(i)
-        val d = digits.getOrNull(i + 1)
-        if (b == c) {
-            if (a == null || a != c) {
-                if (d == null || d != c) {
-                    return true
-                }
-            }
-        }
-    }
-    return false
-}
-
 private fun countValidPasswordsPart1() = (LOW .. HIGH).filter {
     it.isIncreasing() && it.hasRepeatOfAtLeast2()
 }.size
 
 private fun countValidPasswordsPart2() = (LOW .. HIGH).filter {
-    it.isIncreasing() && it.hasRepeatOfExactly2()
+    it.isIncreasing()
+}.filter {
+    val digitCounts = it.digitList().groupingBy { it }.eachCount()
+    digitCounts.any { it.value == 2 }
 }.size
 
 fun main() {
