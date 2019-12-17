bf0f4f87d017e9ca1b23b68f2ce769dbb108a935
Yann Le Gall <ylegall@gmail.com>
2019-12-17 12:55:12 -0800

    adds 2019 day 16 part 1

---
+++ src/main/kotlin/aoc2019/Day16.kt
@@ -0,0 +1,38 @@
+package aoc2019
+
+import util.repeat
+import java.io.File
+import kotlin.math.abs
+
+private val basePattern = sequenceOf(0, 1, 0, -1)
+
+private fun getPatternCoefficients(outputIndex: Int): Sequence<Int> = basePattern
+        .repeat()
+        .flatMap { item ->
+            generateSequence { item }.take(outputIndex + 1)
+        }.drop(1)
+
+private fun computePhase(input: List<Int>): List<Int> {
+    return input.indices.map { index ->
+        val sum = getPatternCoefficients(index)
+                .zip(input.asSequence())
+                .take(input.size).map {
+                    it.first * it.second
+                }.sum()
+        abs(sum % 10)
+    }.toList()
+}
+
+fun computeFirst8DigitsAfter100Phases(input: List<Int>) {
+    var input = input
+    repeat(100) { input = computePhase(input) }
+    val first8Digits = input.take(8).joinToString("")
+    println(first8Digits)
+}
+
+private fun parseInput() = File("inputs/2019/16.txt").readText().trim().map { it - '0' }
+
+fun main() {
+    val input = parseInput()
+    computeFirst8DigitsAfter100Phases(input)
+}
