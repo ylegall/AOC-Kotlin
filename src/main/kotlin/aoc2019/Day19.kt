331fe24e76a97216401a3d298f8a1b4132eecbf7
Yann Le Gall <ylegall@gmail.com>
2019-12-22 08:06:16 -0800

    adds 2019 day 19 part 1

---
+++ src/main/kotlin/aoc2019/Day19.kt
@@ -0,0 +1,41 @@
+package aoc2019
+
+private fun countPointsInRangeOfBeam(codes: Iterable<Long>) {
+    var totalPoints = 0
+
+    for (y in 0 until 50) {
+        for (x in 0 until 50) {
+            if (testTractorBeam(codes, x, y) == 1) {
+                totalPoints++
+                print('#')
+            } else {
+                print('.')
+            }
+        }
+        println()
+    }
+
+    println(totalPoints)
+}
+
+private fun testTractorBeam(codes: Iterable<Long>, x: Int, y: Int): Int {
+    var output = -1L
+    val inputs = sequence {
+        yield(x)
+        yield(y)
+    }.iterator()
+
+    val processor = IntCodeProcessor(
+            codes,
+            inputSupplier = { inputs.next().toLong() },
+            outputConsumer = { output = it }
+    )
+    processor.pauseOnOutput = true
+    processor.run()
+    return output.toInt()
+}
+
+fun main() {
+    val codes = loadIntCodeInstructions("inputs/2019/19.txt")
+    countPointsInRangeOfBeam(codes)
+}
