7f39aa353d20fa1f7006127ca2045e30dee0810e
Yann Le Gall <ylegall@gmail.com>
2019-12-30 20:41:52 -0800

    adds topological sort utilty

---
+++ src/main/kotlin/util/TopologicalSort.kt
@@ -0,0 +1,32 @@
+package util
+
+
+fun <T> topologicalSort(dependencies: Map<T, Iterable<T>>): List<T> {
+    var remainingItems = dependencies.mapValues { it.value.toSet() }
+    val sortedItems = LinkedHashSet<T>(dependencies.keys.size)
+    while (remainingItems.isNotEmpty()) {
+        val (itemsWithoutDependencies, itemsWithDependencies) = remainingItems.entries.partition { it.value.isEmpty() }
+        sortedItems.addAll(itemsWithoutDependencies.map { it.key })
+
+        remainingItems = itemsWithDependencies.associate { (item, dependencies) ->
+            item to dependencies.filter { it !in sortedItems }.toSet()
+        }
+    }
+    return sortedItems.toList()
+}
+
+fun <T> stratifiedTopologicalSort(dependencies: Map<T, Iterable<T>>): List<Set<T>> {
+    var remainingItems = dependencies.mapValues { it.value.toSet() }
+    val sortedItems = LinkedHashSet<T>()
+    val sortedGroups = ArrayList<Set<T>>()
+    while (remainingItems.isNotEmpty()) {
+        val (itemsWithoutDependencies, itemsWithDependencies) = remainingItems.entries.partition { it.value.isEmpty() }
+        val nextItems = itemsWithoutDependencies.map { it.key }
+        sortedGroups.add(nextItems.toSet())
+        sortedItems.addAll(nextItems)
+        remainingItems = itemsWithDependencies.associate { (item, dependencies) ->
+            item to dependencies.filter { it !in sortedItems }.toSet()
+        }
+    }
+    return sortedGroups
+}
