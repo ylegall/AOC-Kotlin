92a0c26a071d5543e8908dfb45901db82fa095ff
Yann Le Gall <ylegall@gmail.com>
2020-01-12 19:41:13 -0800

    adds 2019 day 25 part 1

--- src/main/kotlin/aoc2019/Day25.kt
+++ src/main/kotlin/aoc2019/Day25.kt
@@ -1,110 +1,86 @@
 package aoc2019
 
+import util.arrayDequeOf
 import java.util.ArrayDeque
 import java.util.Scanner
+import kotlin.system.exitProcess
 
 private object Day25 {
     
     private val codes = loadIntCodeInstructions("inputs/2019/25.txt")
     
+    private val preloadedCommands = arrayDequeOf("north", "take dark matter", "east", "east", "take bowl of rice",
+            "west", "south", "take dehydrated water", "north", "west", "north", "east", "south", "take antenna",
+            "west", "take hypercube", "east", "north", "west", "north", "take manifold", "west", "take jam", "east",
+            "east", "take candy cane", "west", "south", "south", "south", "west", "south", "west"
+    )
+    
+    private val allItems = listOf("candy cane", "hypercube", "manifold", "dark matter",
+            "jam", "bowl of rice", "antenna", "dehydrated water"
+    )
+    
     private fun String.expandCommand() = when(this) {
-        "n" -> "north"
-        "e" -> "east"
-        "s" -> "south"
-        "w" -> "west"
-        else -> this
+        "n" -> listOf("north")
+        "e" -> listOf("east")
+        "s" -> listOf("south")
+        "w" -> listOf("west")
+        "drop all" -> allItems.map { "drop $it" }
+        "take all" -> allItems.map { "take $it" }
+        else -> listOf(this)
     }
     
-    fun exploreShip() {
-        val outputParser = OutputParser()
+    fun exploreShipAuto() {
         val scanner = Scanner(System.`in`)
         val inputQueue = ArrayDeque<Int>()
         
         fun getNextCommand(): Long {
-            
             if (inputQueue.isEmpty()) {
-                
-                println("""
-                    room data:
-                        room name: ${outputParser.roomName}
-                        doors    : ${outputParser.doors}
-                        items    : ${outputParser.items}
-                """.trimIndent())
-                
-                if (outputParser.isNotEmpty()) {
-                    outputParser.clear()
+                val commands = if (preloadedCommands.isEmpty()) {
+                    scanner.nextLine().expandCommand()
+                } else {
+                    listOf(preloadedCommands.poll())
                 }
-                val command = scanner.nextLine().expandCommand().map { it.toInt() }
-                inputQueue.apply { addAll(command); add(10) }
+                val charCodes = commands.joinToString("\n").map { it.toInt() }
+                inputQueue.apply { addAll(charCodes); add(10) }
             }
             return inputQueue.poll().toLong()
         }
         
-        fun processOutput(charCode: Long) {
-            print(charCode.toChar())
-            outputParser.append(charCode)
-        }
-        
-        val processor = intCodeProcessor(codes) {
+        intCodeProcessor(codes) {
             inputSupplier = { getNextCommand() }
-            outputConsumer = { processOutput(it) }
+            outputConsumer = { print(it.toChar()) }
+        }.run()
+    }
+
+    fun exploreShipManual() {
+        val scanner = Scanner(System.`in`)
+        val commandHistory = mutableListOf<String>()
+        val inputQueue = ArrayDeque<Int>()
+
+        fun getNextCommand(): Long {
+            if (inputQueue.isEmpty()) {
+
+                val commands = scanner.nextLine().expandCommand()
+                if (commands == listOf("stop")) {
+                    println(commandHistory)
+                    exitProcess(0)
+                }
+                commandHistory.addAll(commands)
+                val charCodes = commands.joinToString("\n").map { it.toInt() }
+                inputQueue.apply { addAll(charCodes); add(10) }
+            }
+            return inputQueue.poll().toLong()
         }
-        processor.run()
+
+        intCodeProcessor(codes) {
+            inputSupplier = { getNextCommand() }
+            outputConsumer = { print(it.toChar()) }
+        }.run()
     }
     
-    private class OutputParser {
-        private val lineBuffer = mutableListOf<String>()
-        private val buffer = StringBuilder()
-        private enum class Mode { NONE, DOORS, ITEMS }
-        private var mode = Mode.NONE
-        var roomName = ""
-        val items = mutableListOf<String>()
-        val doors = mutableListOf<String>()
-        
-        fun append(code: Long) {
-            val char = code.toChar()
-            if (char == '\n') {
-                val lastLine = buffer.toString()
-                if (lastLine.isEmpty()) {
-                    mode = Mode.NONE
-                }
-                
-                when (mode) {
-                    Mode.NONE -> {
-                        if (lastLine.startsWith("==")) {
-                            roomName = lastLine
-                        }
-                        mode = when (lastLine) {
-                            "Doors here lead:" -> Mode.DOORS
-                            "Items here:"      -> Mode.ITEMS
-                            else               -> mode
-                        }
-                    }
-                    Mode.DOORS -> doors.add(lastLine.substring(2))
-                    Mode.ITEMS -> items.add(lastLine.substring(2))
-                }
-                
-                if (lastLine.isNotBlank()) {
-                    lineBuffer.add(lastLine)
-                }
-                buffer.clear()
-            } else {
-                buffer.append(char)
-            }
-        }
-        
-        fun isNotEmpty() = buffer.isNotEmpty() || lineBuffer.isNotEmpty()
-        
-        fun clear() {
-            lineBuffer.clear()
-            buffer.clear()
-            doors.clear()
-            items.clear()
-        }
-    }
 }
 
 fun main() {
-    Day25.exploreShip()
+    Day25.exploreShipAuto()
 }
 
