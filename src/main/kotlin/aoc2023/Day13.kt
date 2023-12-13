4f185b50858dcbc137806efa79888d510e67d142
Yann Le Gall <ylegall@gmail.com>
2023-12-13 13:19:09 -0800

    clean up 2023 day 13

--- src/main/kotlin/aoc2023/Day13.kt
+++ src/main/kotlin/aoc2023/Day13.kt
@@ -24,12 +24,6 @@ fun main() {
         }.reduce { acc, pair -> Pair(acc.first + pair.first, acc.second + pair.second) }
     }
 
-    fun isReflectionAtCol(input: List<String>, col: Int): Boolean {
-        require(col > 0 && col < input[0].length) { "row out of bounds" }
-        val (left, right) = input.partitionAtCol(col)
-        return left.zip(right).all { it.first == it.second }
-    }
-
     fun List<String>.partitionAtRow(row: Int): Pair<List<String>, List<String>> {
         require(row in 1 until size) { "row out of bounds" }
         val bottom = take(row)
@@ -41,47 +35,34 @@ fun main() {
         }
     }
 
-    fun isReflectionAtRow(input: List<String>, row: Int): Boolean {
-        val (bottom, top) = input.partitionAtRow(row)
-        return bottom == top
-    }
-
     fun mismatches(left: List<String>, right: List<String>): Int {
         return left.zip(right).sumOf { (s1, s2) ->
             s1.zip(s2).count { (a, b) -> a != b }
         }
     }
 
+    fun findReflections(input: List<List<String>>, mismatches: Int): Int {
+        return input.sumOf { region ->
+            val rowReflections = (1 until region.size).asSequence().mapNotNull { row ->
+                val (bottom, top) = region.partitionAtRow(row)
+                row.takeIf { mismatches(bottom, top) == mismatches }
+            }.firstOrNull() ?: 0
+
+            val colReflections = (1 until region[0].length).asSequence().mapNotNull { col ->
+                val (left, right) = region.partitionAtCol(col)
+                col.takeIf { mismatches(left, right) == mismatches }
+            }.firstOrNull() ?: 0
+
+            colReflections + 100 * rowReflections
+        }
+    }
+
     fun part1() {
-        val result = input.map { region ->
-            val vertical = (1 until region[0].length).firstOrNull { col -> isReflectionAtCol(region, col) } ?: 0
-            val horizontal = (1 until region.size).firstOrNull { row -> isReflectionAtRow(region, row) } ?: 0
-            Pair(vertical, horizontal)
-        }.sumOf { it.first + 100 * it.second }
-        println(result)
+        println(findReflections(input, 0))
     }
 
     fun part2() {
-        val result = input.sumOf { region ->
-            val singleErrorRows = (1 until region.size).asSequence().mapNotNull { row ->
-                val (bottom, top) = region.partitionAtRow(row)
-                val mismatchCount = mismatches(bottom, top)
-                row.takeIf { mismatchCount == 1 }
-            }.toList()
-
-            val singleErrorCols = (1 until region[0].length).asSequence().mapNotNull { col ->
-                val (left, right) = region.partitionAtCol(col)
-                val mismatchCount = mismatches(left, right)
-                col.takeIf { mismatchCount == 1 }
-            }.toList()
-
-            if (singleErrorRows.isNotEmpty()) {
-                100 * singleErrorRows.first()
-            } else {
-                singleErrorCols.first()
-            }
-        }
-        println(result)
+        println(findReflections(input, 1))
     }
 
     part1()
