946c3e847b386614a4477b28fafa0416b2097d9e
Yann Le Gall <ylegall@gmail.com>
2024-10-18 15:25:56 -0700

    refactor 2019 day 4

--- src/main/kotlin/aoc2019/Day4.kt
+++ src/main/kotlin/aoc2019/Day4.kt
@@ -1,39 +1,40 @@
 package aoc2019
 
 
-private const val LOW = 206938
-private const val HIGH = 679128
-
-private fun Int.digitList(): List<Int> {
-    var number = this
-    val digits = MutableList(6) { 0 }
-    for (i in 5 downTo 0) {
-        digits[i] = number % 10
-        number /= 10
-    }
-    return digits
-}
-
-private fun Int.isIncreasing() = digitList().zipWithNext().all { (first, second) ->
-    first <= second
-}
-
-private fun Int.hasRepeatOfAtLeast2() = digitList().zipWithNext().any { (first, second) ->
-    first == second
-}
-
-private fun countValidPasswordsPart1() = (LOW .. HIGH).filter {
-    it.isIncreasing() && it.hasRepeatOfAtLeast2()
-}.size
-
-private fun countValidPasswordsPart2() = (LOW .. HIGH).filter {
-    it.isIncreasing()
-}.filter {
-    val digitCounts = it.digitList().groupingBy { it }.eachCount()
-    digitCounts.any { it.value == 2 }
-}.size
-
 fun main() {
+
+    val low = 206938
+    val high = 679128
+
+    fun Int.digitList(): List<Int> {
+        var number = this
+        val digits = MutableList(6) { 0 }
+        for (i in 5 downTo 0) {
+            digits[i] = number % 10
+            number /= 10
+        }
+        return digits
+    }
+
+    fun Int.isIncreasing() = digitList().zipWithNext().all { (first, second) ->
+        first <= second
+    }
+
+    fun Int.hasRepeatOfAtLeast2() = digitList().zipWithNext().any { (first, second) ->
+        first == second
+    }
+
+    fun countValidPasswordsPart1() = (low .. high).filter {
+        it.isIncreasing() && it.hasRepeatOfAtLeast2()
+    }.size
+
+    fun countValidPasswordsPart2() = (low .. high).filter {
+        it.isIncreasing()
+    }.filter {
+        val digitCounts = it.digitList().groupingBy { it }.eachCount()
+        digitCounts.any { it.value == 2 }
+    }.size
+
     println(countValidPasswordsPart1())
     println(countValidPasswordsPart2())
 }
