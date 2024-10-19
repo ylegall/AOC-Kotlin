ec9eda16eb2232d092e0bfda9334d1e115291191
Yann Le Gall <ylegall@gmail.com>
2024-10-18 17:26:20 -0700

    refactor 2019 day 6

--- src/main/kotlin/aoc2019/Day6.kt
+++ src/main/kotlin/aoc2019/Day6.kt
@@ -2,59 +2,59 @@ package aoc2019
 
 import java.io.File
 
-private fun parseInput(): Map<String, Set<String>> {
-    return File("inputs/2019/6.txt").useLines { lines ->
-        lines.map { it.split(")") }.groupBy(
-                { it[0] },
-                { it[1] }
-        ).mapValues { it.value.toSet() }
-    }
-}
+fun main() {
 
-private fun countOrbits(
+    fun parseInput(): Map<String, Set<String>> {
+        return File("input.txt").useLines { lines ->
+            lines.map { it.split(")") }.groupBy(
+                    { it[0] },
+                    { it[1] }
+            ).mapValues { it.value.toSet() }
+        }
+    }
+
+    fun countOrbits(
         tree: Map<String, Set<String>>,
         currentNode: String = "COM",
         depth: Int = 0
-): Int {
-    val children = tree[currentNode] ?: emptySet()
-    return depth + children.map { nextNode -> countOrbits(tree, nextNode, depth + 1) }.sum()
-}
-
-private fun findPath(
-        start: String,
-        target: String,
-        tree: Map<String, Set<String>>,
-        currentNode: String = start,
-        path: List<String> = listOf(start)
-): List<String> {
-    return if (currentNode == target) {
-        path
-    } else {
+    ): Int {
         val children = tree[currentNode] ?: emptySet()
-        children.map { nextNode ->
-            findPath(start, target, tree, nextNode, path + nextNode)
-        }.firstOrNull {
-            it.isNotEmpty()
-        } ?: emptyList()
+        return depth + children.sumOf { nextNode -> countOrbits(tree, nextNode, depth + 1) }
     }
-}
 
-private fun countTotalOrbits() {
+    fun findPath(
+            start: String,
+            target: String,
+            tree: Map<String, Set<String>>,
+            currentNode: String = start,
+            path: List<String> = listOf(start)
+    ): List<String> {
+        return if (currentNode == target) {
+            path
+        } else {
+            val children = tree[currentNode] ?: emptySet()
+            children.map { nextNode ->
+                findPath(start, target, tree, nextNode, path + nextNode)
+            }.firstOrNull {
+                it.isNotEmpty()
+            } ?: emptyList()
+        }
+    }
+
+    fun countTotalOrbits(orbits: Map<String, Set<String>>) {
+        println(countOrbits(orbits))
+    }
+
+    fun countOrbitalTransfers(orbits: Map<String, Set<String>>) {
+        val ourPath = findPath("COM", "YOU", orbits)
+        val santasPath = findPath("COM", "SAN", orbits)
+        val commonAncestor = ourPath.zip(santasPath).takeWhile { it.first == it.second }.last().first
+        val ourOrbitLength = ourPath.dropWhile { it != commonAncestor }.size - 2
+        val santasOrbitLength = santasPath.dropWhile { it != commonAncestor }.size - 2
+        println(ourOrbitLength + santasOrbitLength)
+    }
+
     val orbits = parseInput()
-    println(countOrbits(orbits))
-}
-
-private fun countOrbitalTransfers() {
-    val orbits = parseInput()
-    val ourPath = findPath("COM", "YOU", orbits)
-    val santasPath = findPath("COM", "SAN", orbits)
-    val commonAncestor = ourPath.zip(santasPath).takeWhile { it.first == it.second }.last().first
-    val ourOrbitLength = ourPath.dropWhile { it != commonAncestor }.size - 2
-    val santasOrbitLength = santasPath.dropWhile { it != commonAncestor }.size - 2
-    println(ourOrbitLength + santasOrbitLength)
-}
-
-fun main() {
-    countTotalOrbits()
-    countOrbitalTransfers()
+    countTotalOrbits(orbits)
+    countOrbitalTransfers(orbits)
 }
