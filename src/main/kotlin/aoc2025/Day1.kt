6870815c3622fe5258a51ed36d6dfd8720626ca6
Yann Le Gall <ylegall@gmail.com>
2025-12-02 18:44:41 -0800

    adds 2025 day 1

---
+++ src/main/kotlin/aoc2025/Day1.kt
@@ -0,0 +1,46 @@
+package aoc2025
+
+import java.io.File
+
+private object Day1 {
+
+    const val DIAL_SIZE = 100
+    const val START_POSITION = 50
+
+    fun parseInput() = File("input.txt").useLines { lines ->
+        lines.map { line ->
+            val delta = line.drop(1).toInt()
+            if (line.startsWith('L')) -delta else delta
+        }.toList()
+    }
+
+    fun part1(rotations: List<Int>) {
+        val password = rotations.scan(START_POSITION) { position, rotation ->
+            (position + rotation) % DIAL_SIZE
+        }.count { it == 0 }
+        println(password)
+    }
+
+    fun part2(deltas: List<Int>) {
+        var count = 0
+        var pos = START_POSITION
+        for (delta in deltas) {
+            val (newDelta, dist) = if (delta < 0) {
+                -delta to if (pos == 0) 100 else pos
+            } else {
+                delta to (100 - pos)
+            }
+            if (newDelta >= dist) {
+                count += (1 + (newDelta-dist)/DIAL_SIZE)
+            }
+            pos = (pos + delta).mod(DIAL_SIZE)
+        }
+        println(count)
+    }
+}
+
+fun main() {
+    val rotations = Day1.parseInput()
+    Day1.part1(rotations)
+    Day1.part2(rotations)
+}
