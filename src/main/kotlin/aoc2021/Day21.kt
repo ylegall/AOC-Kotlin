828b29732ddbcc3d3ef1cfa0b357c8c8fbdd1d00
Yann Le Gall <ylegall@gmail.com>
2025-12-18 17:01:20 -0800

    refactor 2021 day 21

--- src/main/kotlin/aoc2021/Day21.kt
+++ src/main/kotlin/aoc2021/Day21.kt
@@ -1,77 +1,111 @@
 package aoc2021
 
+import util.replace
+import java.io.File
+import kotlin.math.max
 
-fun main() {
+private object Day21 {
 
-    val start = Pair(4, 8)
-//    val start = Pair(7, 10)
+    const val SIZE = 10
+
+    fun parseInput(): Pair<Int, Int> {
+        return File("input.txt").readLines().let { lines ->
+            Pair(
+                lines.first().let { it.drop(it.lastIndexOf(' ')+1).toInt() },
+                lines.last().let { it.drop(it.lastIndexOf(' ')+1).toInt() }
+            )
+        }
+    }
+
+    fun simulateGameDeterministic(
+        input: Pair<Int, Int>,
+        goal: Int
+    ): Int {
+        var turn = 0
+        var rolls = 0
+        val positions = mutableListOf(input.first, input.second)
+        val scores = mutableListOf(0, 0)
+        var diePosition = 0
+        while (scores.none { it >= goal }) {
+            val index = turn % 2
+            val rollValue = (diePosition ..diePosition + 2).map { it % 100 + 1 }
+            diePosition = rollValue.last()
+            positions[index] = (positions[index] + rollValue.sum() - 1) % SIZE + 1
+            scores[index] += positions[index]
+            turn++
+            rolls += 3
+        }
+        return scores.minOrNull()!! * rolls
+    }
 
     data class Player(
-        var pos: Int,
-        var score: Int = 0
+        val position: Int,
+        val score: Int = 0
     )
 
-    fun part1(): Long {
-        var turn = 0
-        val players = listOf(
-            Player(start.first - 1),
-            Player(start.second - 1)
-        )
-        var d = 0
-
-        while (players.all { it.score < 1000 }) {
-            val player = players[turn % 2]
-            val rollValue = (d .. d + 2).asSequence()
-                .map { (it % 100) + 1 }.sum()
-            d = (d + 3) % 100
-            player.pos = (player.pos + rollValue) % 10
-            player.score += player.pos + 1
-            turn++
+    fun tripleSumFrequencies(): Map<Int, Int> {
+        val counts = mutableMapOf<Int, Int>()
+        for (i in 1 .. 3) {
+            for (j in 1 .. 3) {
+                for (k in 1 .. 3) {
+                    val sum = i + j + k
+                    counts[sum] = (counts[sum] ?: 0) + 1
+                }
+            }
         }
-
-        val losingScore = players.minOf { it.score }
-        println("turn = $turn")
-        println("lossing score = $losingScore")
-        println("rolls ${turn * 3}")
-        return losingScore * (turn * 3L)
+        return counts
     }
 
-    class GameState(
-        val players: Pair<Player, Player>,
-        val d: Int = 0,
-        val turn: Int = 0,
+    data class State(
+        val players: List<Player>,
+        val turn: Int
     )
 
+    fun simulateGameQuantum(
+        input: Pair<Int, Int>,
+        goal: Int
+    ): Pair<Long, Long> {
+        val tripleSumFrequencies = tripleSumFrequencies()
+        val cache = mutableMapOf<State, Pair<Long, Long>>()
 
-    fun part2() {
-
-        fun playRecursive(game: GameState): Pair<Long, Long> {
-            val (p1, p2) = game.players
-            if (p1.score >= 21) {
-                return 1L to 0L
+        fun recurse(state: State): Pair<Long, Long> {
+            return when {
+                state.players[0].score >= goal -> 1L to 0L
+                state.players[1].score >= goal -> 0L to 1L
+                else -> cache.getOrPut(state) {
+                    val playerIndex = state.turn % 2
+                    val player = state.players[playerIndex]
+                    tripleSumFrequencies.map { (rollSum, count) ->
+                        val newPosition = (player.position + rollSum - 1) % SIZE + 1
+                        val newScore = player.score + newPosition
+                        val newPlayers = state.players.replace(playerIndex, Player(newPosition, newScore))
+                        val newState = State(newPlayers, state.turn + 1)
+                        recurse(newState).let {
+                            it.first * count to it.second * count
+                        }
+                    }.reduce { a, b ->
+                        a.first + b.first to a.second + b.second
+                    }
+                }
             }
-            if (p2.score >= 21) {
-                return 0L to 1L
-            }
-
-            val player = if (game.turn % 2 == 0) p1 else p2
-
-            val totalWins = (1 .. 3).map { rollValue ->
-                val newPosition = (player.pos + rollValue) % 10
-                val newScore = player.score + newPosition + 1
-                val newPlayer = Player(newPosition, newScore)
-                val newPlayers = if (game.turn % 2 == 0) newPlayer to p2 else p1 to newPlayer
-                val newGame = GameState(newPlayers, game.turn + 1)
-                playRecursive(newGame)
-            }.reduce { a, b -> a.first + b.first to a.second + b.second }
-            return totalWins
         }
 
-        val game = GameState(Player(start.first - 1) to Player(start.second - 1))
-        val totals = playRecursive(game)
-        println(totals)
+        val initialState = State(listOf(Player(input.first), Player(input.second)), 0)
+        return recurse(initialState)
     }
 
-//    println(part1())
-    println(part2())
+    fun part1(input: Pair<Int, Int>) {
+        println(simulateGameDeterministic(input, 1000))
+    }
+
+    fun part2(input: Pair<Int, Int>) {
+        val wins = simulateGameQuantum(input, 21)
+        println(max(wins.first, wins.second))
+    }
+}
+
+fun main() {
+    val input = Day21.parseInput()
+    Day21.part1(input)
+    Day21.part2(input)
 }
